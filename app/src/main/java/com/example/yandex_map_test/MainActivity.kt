d426d03710ed305da60edcb3ba26c988b3464d08
Андрей Александрович <res1.main@gmail.com>
2022-05-05 22:08:32 +0300

    added viewBinding

--- app/src/main/java/com/example/yandex_map_test/MainActivity.kt
+++ app/src/main/java/com/example/yandex_map_test/MainActivity.kt
@@ -2,23 +2,24 @@ package com.example.yandex_map_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.yandex_map_test.databinding.ActivityMainBinding
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var mapView: MapView
+
+    private val binding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapKitFactory.setApiKey("03cc843d-28b1-4751-adf3-4b4213eec37c");
         MapKitFactory.initialize(this);
-        setContentView(R.layout.activity_main)
-        mapView = findViewById(R.id.mapview)
-        mapView.map.move(
+        binding.mapview.map.move(
             CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
             Animation(Animation.Type.SMOOTH, 0F),
             null
@@ -27,13 +28,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        mapView.onStart()
+        binding.mapview.onStart()
         MapKitFactory.getInstance().onStart()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView.onStop()
+        binding.mapview.onStop()
         MapKitFactory.getInstance().onStop()
     }
 }
