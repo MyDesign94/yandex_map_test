1f5e59ec7244330f17c3e8b14e9b5493755815f4
Андрей Александрович <res1.main@gmail.com>
2022-05-05 22:19:54 +0300

    added viewBinding

--- app/src/main/java/com/example/yandex_map_test/MainActivity.kt
+++ app/src/main/java/com/example/yandex_map_test/MainActivity.kt
@@ -10,21 +10,24 @@ import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+
 
 
     private val binding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MapKitFactory.setApiKey("03cc843d-28b1-4751-adf3-4b4213eec37c");
-        MapKitFactory.initialize(this);
+        MapKitFactory.setApiKey("03cc843d-28b1-4751-adf3-4b4213eec37c")
+        MapKitFactory.initialize(this)
+        setContentView(R.layout.activity_main)
         binding.mapview.map.move(
             CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
             Animation(Animation.Type.SMOOTH, 0F),
             null
         )
     }
+
 
     override fun onStart() {
         super.onStart()
